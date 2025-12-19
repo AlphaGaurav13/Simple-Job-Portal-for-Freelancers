bdcbb0faf435ccc0337c78308edb219d3a2048af
Gaurav132005 <gauravkkr345@gmail.com>
2025-12-19 15:45:34 +0530

    Changed Folder name From Gaurav to Codebase

--- GauravWork/freeLance/src/data/categories.js
+++
@@ -1,9 +0,0 @@
-export const categories = [
-  "All",
-  "Logo Design",
-  "Branding Services",
-  "Social Media Design",
-  "Website Design",
-  "Illustrations",
-  "Packaging Design",
-];
--- GauravWork/freeLance/src/services/serviceApi.js
+++
@@ -1,7 +0,0 @@
-const API_URL = import.meta.env.VITE_API_URL;
-
-export async function fetchServices() {
-  const res = await fetch(`${API_URL}/api/services`);
-  if (!res.ok) throw new Error("Failed to fetch services");
-  return res.json();
-}
